b37e061703162df38423d8f5cbdc59b57cd356c9
yogeshwarreddy2002 <yogeshwarreddy416@gmail.com>
2025-10-30 23:41:57 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,30 @@
+import streamlit as st
+st.title("Jemimah Rodrigues")
+f1,f2,f3,=st.tabs(["Home","image","Achievements"])
+with f1:
+    if f1:
+        st.subheader("Jemimah Rodrigues - The Rising Star of Indian")
+        st.write("Jemimah Rodrigues is an Indian cricketer who plays for the India women's national cricket team as a top-order batter and all-rounder. She recently scored an unbeaten 127 to help India achieve a record-breaking successful run-chase against Australia in the semi-final of the ICC Women's Cricket World Cup 2025.")             
+        st.write("Born on September 5, 2000, in Mumbai, Maharashtra")
+        st.markdown("**Early Life and Career:**")
+        st.markdown("""ICC Women's World Cup 2025 Semi-Final (October 2025): Rodrigues scored an unbeaten 127 to power India to a record-breaking successful chase of 339 runs against Australia, securing their spot in the final.
+                     Shiv Chhatrapati State Sports Award (April 2025): She was honored with Maharashtra's highest sporting award.
+                     Women's Tri-Nation Series Final (May 2025): She won the Player of the Match award for her performance against South Africa""")
+        st.markdown("**Playing Style:**")
+        st.write("Rodrigues is known for her aggressive batting style, ability to play both pace and spin bowling, and her versatility as an all-rounder. She is also a brilliant fielder, often seen taking spectacular catches.")
+with f2:
+    if f2:
+        st.image("https://images.moneycontrol.com/static-mcnews/2023/02/Jemimah-Rodrigues.jpeg", caption="Jemimah Rodrigues",width=500)
+        st.image("https://static.toiimg.com/thumb/msid-118086950,width-400,resizemode-4/118086950.jpg")
+with f3:
+    if f3:
+        st.subheader("Achievements of Jemimah Rodrigues")
+        st.markdown("""- Scored an unbeaten 127 in the ICC  Women's Cricket World Cup 2025 semi-final against Australia.
+                     - Received the Shiv Chhatrapati State Sports Award in April 2025""")
+        st.markdown("- Won the Player of the Match award in the Women's Tri-Nation Series Final against South Africa in May 2025")  
+        st.markdown("- Played a crucial role in India's successful run-chase of 339 runs against Australia in the   ICC Women's Cricket World Cup 2025 semi-final.")
+        st.markdown("- Known for her aggressive batting style and versatility as an all-rounder.")  
+        st.markdown("- Recognized as one of the rising stars in Indian  women's cricket.")
+        st.markdown("- Has represented India in various international tournaments and leagues.")    
+        st.markdown("- A brilliant fielder, often taking spectacular catches on the field.")    
+        st.markdown("- Inspires young girls to pursue cricket and sports in general.")
